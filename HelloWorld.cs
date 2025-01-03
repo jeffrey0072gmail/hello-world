7c0a83ebd7f2115a42c548a635f4df07613f6cfa
jeffrey0072gmail <jeffrey0072@gmail.com>
2025-01-03 12:32:46 -0500

    Add basic "Hello, World!" console application in C#
    Add basic "Hello, World!" console application in C#
    
    - Created a new C# console application project.
    - Implemented the Main method to print "Hello, World!" to the console.
    - Added comments to explain each part of the code.

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print "Hello, World!" to the console
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
